5a732e5a3eedbc108becdc181fa8d7777bfe9863
doRA <dev9876dora@gmail.com>
2019-12-13 16:20:26 +0900

    Update vim_install.sh

--- vim_install.sh
+++ vim_install.sh
@@ -14,8 +14,12 @@ echo "${password}" | sudo -S apt -y upgrade
 echo "${password}" | sudo -S apt install -y build-essential ncurses-dev lua5.2 lua5.2-dev luajit python-dev python3-dev ruby-dev
 
 cd ${DOWNLOAD_DIR}
-echo "${password}" | sudo -S git clone https://github.com/vim/vim
+echo "${password}" | sudo -S git clone --depth 1 https://github.com/vim/vim
 cd vim
+
+cd src
+make distclean
+cd ..
 
 echo "${password}" | sudo -S ./configure --with-features=huge --enable-multibyte --enable-luainterp=dynamic --enable-gpm --enable-cscope --enable-fontset --enable-fail-if-missing --prefix=/usr/local --enable-pythoninterp=dynamic --enable-python3interp=dynamic --enable-rubyinterp=dynamic
 
